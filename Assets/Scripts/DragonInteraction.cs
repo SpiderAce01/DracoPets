19d36583d7634b3c98de08a097c81bc376b133f5
Samantha Greig <samanthagreig29@hotmail.com>
2022-03-09 02:06:28 +1100

    Final Changes

--- Assets/Scripts/DragonInteraction.cs
+++ Assets/Scripts/DragonInteraction.cs
@@ -22,10 +22,13 @@ public class DragonInteraction : MonoBehaviour
 
     public bool d1 = false;
     public bool d2 = false;
+    public bool d3 = false;
+    public bool d4 = false;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        interactPoint = GameObject.Find("InteractPoint");
     }
 
     private void Update()
@@ -41,6 +44,26 @@ public class DragonInteraction : MonoBehaviour
         }
 
         if (d2 == true)
+        {
+            cooldown += Time.deltaTime;
+
+            if (cooldown >= 5)
+            {
+                control.enabled = true;
+            }
+        }
+
+        if (d3 == true)
+        {
+            cooldown += Time.deltaTime;
+
+            if (cooldown >= 5)
+            {
+                control.enabled = true;
+            }
+        }
+
+        if (d4 == true)
         {
             cooldown += Time.deltaTime;
 
@@ -88,22 +111,48 @@ public class DragonInteraction : MonoBehaviour
                 anim.SetInteger("choiceAnim", 0);
             }
         }
+
+        if (gameObject.tag == "Dragon3")
+        {
+            d3 = true;
+            control.enabled = false;
+
+            cooldown = 0;
+
+            agent.SetDestination(interactPoint.transform.position);
+
+            if (showOptions == false)
+            {
+                anim.SetInteger("choiceAnim", 0);
+            }
+        }
+
+        if (gameObject.tag == "Dragon4")
+        {
+            d4 = true;
+            control.enabled = false;
+
+            cooldown = 0;
+            cooldown += Time.deltaTime;
+
+            if (cooldown >= 5)
+            {
+                control.enabled = true;
+            }
+
+            agent.SetDestination(interactPoint.transform.position);
+
+            if (showOptions == false)
+            {
+                anim.SetInteger("choiceAnim", 0);
+            }
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Interact")
         {
-            if (d1 == true)
-            {
-                showOptions = true;
-            }
-
-            if(d2 == true)
-            {
-                showOptions1 = true;
-            }
-
             anim.SetInteger("choiceAnim", 1);
         }
     }
