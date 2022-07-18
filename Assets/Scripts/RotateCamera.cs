1653b74b455754d7c78cc21dd4b7687463998f97
Hayden <whitefiregames1@hotmail.com>
2022-07-18 11:29:12 +1000

    camera fix

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,33 +4,45 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float cSpeed;
+    public float rotationY = 0f;
+    public float rotationX = 0f;
+    public float speed;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.left * cSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(Vector3.right * cSpeed * Time.deltaTime);
+            rotationY += speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.down * cSpeed * Time.deltaTime);
+            rotationY -= speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        rotationY = Mathf.Clamp(rotationY, 80, 180);
+        var rotY = transform.localEulerAngles;
+        rotY.y = rotationY;
+        transform.localEulerAngles = -rotY;
+
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Rotate(Vector3.up * cSpeed * Time.deltaTime);
+            rotationX += speed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rotationX -= speed * Time.deltaTime;
+        }
+
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        var rotX = transform.localEulerAngles;
+        rotX.x = rotationX;
+        transform.localEulerAngles = -rotX;
     }
 }
